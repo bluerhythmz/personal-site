ba43cb4d4c8f45121e34d4dc466caade7314558b
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2020-11-28 13:23:49 -0800

    navigation fix

--- mobile.js
+++ mobile.js
@@ -4,7 +4,10 @@ const cancel = document.getElementById('cancel')
 const darken = document.getElementById('darken')
 
 menu.addEventListener('click', () => {
-    nav.classList.add('reveal')
+    nav.classList.add('pop')
+    setTimeout(() => {
+        nav.style.right = "0"
+    }, 100)
     menu.style.opacity = "0"
     darken.classList.add('shade')
     darken.style.zIndex = "1"
@@ -12,7 +15,10 @@ menu.addEventListener('click', () => {
 
 cancel.addEventListener('click', () => {
     nav.classList.remove('reveal')
-    nav.classList.remove('pop')
+    nav.style.right = "-400px"
+    setTimeout(() => {
+        nav.classList.remove('pop')
+    }, 150)
     menu.style.opacity = "1"
     darken.classList.remove('shade')
     darken.style.zIndex = "-1"
