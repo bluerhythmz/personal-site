a10df2b40fc1cf44ebaf49e88330417c4bbb8a68
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2020-11-28 12:55:37 -0800

    navigation update desktop + mobile

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-const burger = document.getElementsByClassName('toggle-button')[0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
+const menu = document.getElementsByClassName('toggle-button-container')[0]
+const nav = document.querySelector('nav')
+const cancel = document.getElementById('cancel')
+const darken = document.getElementById('darken')
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
+menu.addEventListener('click', () => {
+    nav.classList.add('reveal')
+    menu.style.opacity = "0"
+    darken.classList.add('shade')
+    darken.style.zIndex = "1"
+})
+
+cancel.addEventListener('click', () => {
+    nav.classList.remove('reveal')
+    menu.style.opacity = "1"
+    darken.classList.remove('shade')
+    darken.style.zIndex = "-1"
 })
